30f2d546b575927939b02a8b3c1d3fd48b27e873
Sayer Tindall <sayer.tindall@gmail.com>
2019-11-30 17:12:53 -0800

    Fixed styling issues, setup webpack, rendered HelloWorld with React

---
+++ public/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { VictoryChart, VictoryGroup, VictoryVoronoiContainer,
+  VictoryLine, VictoryTooltip, VictoryScatter } from 'victory';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>HelloWorld</h1>
+    )
+  }
+}
+
+
+export default App;
