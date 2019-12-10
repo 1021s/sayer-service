4f02161a2c7e9bf72ea30983f01b704973501117
sayertindall <34878057+sayertindall@users.noreply.github.com>
2019-12-09 16:15:58 -0800

    Update seed.js

--- server/db/seed.js
+++ server/db/seed.js
@@ -6,18 +6,20 @@ let seedData = [
 ];
 
 // eslint-disable-next-line func-names
-Number.prototype.pad = (size) => {
-  let s = String(this);
-  while (s.length < (size || 2)) { s = '0' + s; }
-  return s;
-};
+
 // TODO turn to function
 for (let i = 0; i < 100; i++) {
   const zip = faker.address.zipCode('#####');
   const price = faker.commerce.price(100000, 1000000);
   const pastDate = faker.date.past(20);
+  let id = i;
+  if (id.toString().length < 2) {
+    id = `00${id.toString()}`;
+  } else {
+    id = `0${id.toString()}`;
+  }
   const houseObj = {
-    Listing_id: (i).pad(3),
+    Listing_id: id,
     initial_cost: price,
     last_sold: pastDate,
     zip: zip,
